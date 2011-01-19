e74abe3fd94598e4faeafcfe97966bbf1ee9a131
Drazzle Bay <broodsoft@gmail.com>
2011-01-19 11:15:52 -0500

    Fixed imports

--- src/com/broodsoft/venture/jpa/manager/PersonManager.java
+++ src/com/broodsoft/venture/jpa/manager/PersonManager.java
@@ -10,9 +10,7 @@ import javax.persistence.criteria.Root;
 
 import com.broodsoft.brew.db.jpa.JPAQueryAdapter;
 import com.broodsoft.brew.doc.CodeAuthor;
-import com.broodsoft.venture.jpa.*;
-import com.broodsoft.venture.jpa.model.Person;
-import com.broodsoft.venture.jpa.model.PhoneNumber;
+import com.broodsoft.venture.jpa.model.*;
 
 @CodeAuthor(first = "Drazzle", last = "Bay")
 public class PersonManager extends JPAQueryAdapter
